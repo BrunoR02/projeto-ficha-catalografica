a6d2d0ef3797cdbc1c59a54da08d97ef96980aad
Bruno Lucas Ribeiro Santos <brunolucas23@gmail.com>
2024-11-03 22:40:27 -0300

    Ajuste implementação sass

--- src/app/formulario-ficha/page.tsx
+++ src/app/formulario-ficha/page.tsx
@@ -3,7 +3,7 @@ import MainTitle from "@/component/texts/MainTitle"
 export default function FormularioPage() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <MainTitle text="Criar Ficha Catalográfica" />
     </main>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MainTitle from "@/component/texts/MainTitle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <MainTitle text="Criar Ficha Catalográfica" />
       <FichaComp />
     </main>
